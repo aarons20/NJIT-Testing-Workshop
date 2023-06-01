65df77ec07abb50aa530ced526a40db56119c698
Jonathan Fernezan <Jonathan_e_fernezan@glic.com>
2023-06-01 10:38:27 -0400

    Adding new files

---
+++ Exceptions.py
@@ -0,0 +1,11 @@
+class BalanceRetrievealException(Exception):
+    'Balance Retrieval Error - 901'
+    pass
+
+class InternalDepositException(Exception):
+    'Internal Deposit Error - 212'
+    pass
+
+class WithdrawlException(Exception):
+    'Withdrawal Error - 787'
+    pass
