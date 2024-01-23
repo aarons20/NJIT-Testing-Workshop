77617d06a64ba3e9e8a526747521a4c2b458255e
PeePeePirate <88449209+PeePeePirate@users.noreply.github.com>
2024-01-23 11:49:03 -0500

    Update tests_bank.py

--- tests_bank.py
+++ tests_bank.py
@@ -7,4 +7,9 @@ import coverage
 def test_view_balance_success():
     bank_account = BankAccount(username='name', balance=100)
     actual_balance = bank_account.view_balance()
-    assert actual_balance == 100
+    assert actual_balance == 100\
+
+def test_view_balance_unsuccessful():
+    bank_account = BankAccount(username='name', balance=-100)
+    with pytest.raises(BalanceRetrievalException):
+        bank_account.view_balance()
